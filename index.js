50334a1727b92f6a1c13aff5b4ff147a44310177
Prayash Shrestha <xrestha17@gmail.com>
2023-12-11 14:52:14 -0600

    javascript classes

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-let myName = "Prayash";
-console.log(myName.slice(0, 1));
+class Pizza {
+  constructor(size, crust) {
+    this.size = size;
+    this.crust = crust;
+  }
+  get pizzaCrust() {
+    return this.crust;
+  }
+  set pizzaCrust(crust) {
+    this.crust = crust;
+  }
+  bake() {
+    console.log(`Baking a ${this.size} ${this.crust} Pizza`);
+  }
+}
+
+class SpecialityPizza extends Pizza {
+  constructor(size, crust) {
+    super(size, crust);
+    this.month = "December";
+  }
+  specialPizza() {
+    console.log(
+      `This ${this.size} ${this.crust} pizza is only available in ${this.month}`
+    );
+  }
+}
+
+const specialPizzaObj = new SpecialityPizza("medium", "thin");
+specialPizzaObj.specialPizza();
