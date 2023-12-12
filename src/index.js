fd8e0603d747494696de0d59b15a9ef831f4dbd3
Prayash Shrestha <xrestha17@gmail.com>
2023-12-11 19:38:33 -0600

    learning typescript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
-var a = 10;
-var b = 20;
-var sum = function (a, b) {
-    return a + b;
-};
-console.log(sum(5, 5));
+var Animal = /** @class */ (function () {
+    function Animal(name, legs) {
+        this.name = name;
+        this.legs = legs;
+        (this.name = name), (this.legs = legs);
+    }
+    Animal.prototype.getLegs = function () {
+        return this.legs;
+    };
+    return Animal;
+}());
+var doggy = new Animal("Super", 4);
+console.log(doggy.name);
+// type One = string;
+// type Two = string | number;
+// let a: One = "Hello";
+// let b = a as Two;
+// console.log(b);
+// let data: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const total = (...data: number[]): number => {
+//   return data.reduce((acc, curr) => acc + curr);
+// };
+// console.log(total(...data));
+// let a: number = 2;
+// let b: number = 3;
+// const sum = (a: number, b: number): number => {
+//   return a + b;
+// };
+// console.log(sum(a, b));
+// type Person = {
+//   name?: string;
+//   age: number;
+//   hobbies: (string | number)[];
+// };
+// let person1: Person = {
+//   name: "Prayash",
+//   age: 26,
+//   hobbies: ["a", "b", 1, 2],
+// };
+// let person2: Person = {
+//   name: "Ashmita",
+//   age: 25,
+//   hobbies: ["c", "d", 3, 4],
+// };
+// const displayName = (personDetail: Person) => {
+//   console.log(
+//     `My name is ${personDetail.name?.toUpperCase()} and I am ${
+//       personDetail.age
+//     } years old.`
+//   );
+// };
+// displayName(person1);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,63 @@
-let a: number = 10;
-let b: number = 20;
+class Animal {
+  constructor(public name: string, private legs: number) {
+    (this.name = name), (this.legs = legs);
+  }
+  public getLegs(): number {
+    return this.legs;
+  }
+}
 
-const sum = (a: number, b: number) => {
-  return a + b;
-};
-console.log(sum(5, 5));
+const doggy = new Animal("Super", 4);
+console.log(doggy.name);
+
+// type One = string;
+// type Two = string | number;
+
+// let a: One = "Hello";
+// let b = a as Two;
+
+// console.log(b);
+
+// let data: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const total = (...data: number[]): number => {
+//   return data.reduce((acc, curr) => acc + curr);
+// };
+
+// console.log(total(...data));
+
+// let a: number = 2;
+// let b: number = 3;
+
+// const sum = (a: number, b: number): number => {
+//   return a + b;
+// };
+
+// console.log(sum(a, b));
+
+// type Person = {
+//   name?: string;
+//   age: number;
+//   hobbies: (string | number)[];
+// };
+
+// let person1: Person = {
+//   name: "Prayash",
+//   age: 26,
+//   hobbies: ["a", "b", 1, 2],
+// };
+// let person2: Person = {
+//   name: "Ashmita",
+//   age: 25,
+//   hobbies: ["c", "d", 3, 4],
+// };
+
+// const displayName = (personDetail: Person) => {
+//   console.log(
+//     `My name is ${personDetail.name?.toUpperCase()} and I am ${
+//       personDetail.age
+//     } years old.`
+//   );
+// };
+
+// displayName(person1);
