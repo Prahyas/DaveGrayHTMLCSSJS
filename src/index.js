44aae736648c5615804947b5c8aa12acb0050e7e
Prayash Shrestha <xrestha17@gmail.com>
2024-02-21 19:55:47 -0600

    minor change

--- src/index.js
+++ src/index.js
@@ -181,4 +181,4 @@ const alertFunction = (e, tag) => {
   alert(`${tag} : ${e.target.textContent}`);
 };
 
-outer.addEventListener("click", (e) => alertFunction(e, "outer"));
+outer.addEventListener("clicks", (e) => alertFunction(e, "outer"));
